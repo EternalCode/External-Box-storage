0f606870c12268ff63a93ca1bfb4f5420fbe5335
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2018-01-06 10:01:52 -0500

    Convert save data into pokemon base

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,37 @@ function get_sector_by_id(id) {
     return -1;
 }
 
+/* Convert u8 array into Pokemon object */
+function array_to_pokemon() {
+    let blob = [];
+    for (let i = 5; i <= 13; i++) {
+        let sector_id = get_sector_by_id(i);
+        // create PC Box blob
+        let size = sector_properties[i - 5].size;
+        if (i == 5) {
+            blob = blob.concat(save_sectors[sector_id].data.slice(4, size));
+        } else if(i == 13) {
+            // don't copy wallpapers
+            blob = blob.concat(save_sectors[sector_id].data.slice(0, size - 140));
+        } else {
+            blob = blob.concat(save_sectors[sector_id].data.slice(0, size));
+        }
+    }
+
+    let box_id = -1;
+    for (let i = 0; i < 30 * 14; i ++) {
+        if (i % 30 == 0)
+            box_id++;
+        let t = new PokemonBase(blob.slice(80 * i, 80 * i + 80));
+        if ((t.pid != "0")) {
+            pc_box.push(t);
+        } else {
+            pc_box.push(empty_mon());
+        }
+    }
+}
+
+
 /* Convert save file into sector objects */
 function array_to_sectors(array) {
     for (let sector = 0; sector < sector_count; sector++) {

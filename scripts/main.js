74aeeffdbc7a1da0ecf6b5e3920d8730097c4a0d
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2018-01-06 10:01:19 -0500

    sector to array

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,30 @@ const sector_count = 32;
 let save_sectors = [];
 let pc_box = [];
 
+function get_sector_by_id(id) {
+    for (let sector = 0; sector <= 13; sector++) {
+        let sector_id = parseInt(save_sectors[sector].id, 16);
+        if (sector_id == id)
+            return sector;
+    }
+    return -1;
+}
+
+/* Convert save file into sector objects */
+function array_to_sectors(array) {
+    for (let sector = 0; sector < sector_count; sector++) {
+        let sector_start = sector * 4096;
+        save_sectors[sector] =
+        {
+            data: array.slice(sector_start, sector_start + 4084),
+            id: half_word(array.slice(sector_start + 4084, sector_start + 4086)),
+            checksum: half_word(array.slice(sector_start + 4086, sector_start + 4088)),
+            security: word(array.slice(sector_start + 4088, sector_start + 4092)),
+            counter: word(array.slice(sector_start + 4092, sector_start + 4096)),
+        }
+        console.log(save_sectors[sector]);
+    }
+}
 
 
 /* Read save file */
